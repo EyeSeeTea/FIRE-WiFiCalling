47e952242329b6eb293b29987287b8a896ca3afd
Jordi <jordi.burguet.castell@gmail.com>
2017-07-04 15:16:49 +0200

    call.*: replace some images with ionic icons and better use the ionic grid
    Following suggestion by Murhaf,
    https://github.com/EyeSeeTea/FIRE-WiFiCalling/pull/19/files/e64e3e82d220006e2a26cbd9c722c746af10d5c2#r125370428
    Thanks!

--- src/pages/call/call.ts
+++ src/pages/call/call.ts
@@ -10,7 +10,6 @@ import { CallingPage } from './calling';
 })
 export class CallPage {
     phoneNumber: string = '';
-    callButtonHidden: boolean = true;
 
     constructor(public navCtrl: NavController,
                 public navParams: NavParams,
@@ -19,22 +18,18 @@ export class CallPage {
 
     add(n: string) {
         this.phoneNumber += n;
-        this.callButtonHidden = false;
     }
 
     removeLast() {
         var len = this.phoneNumber.length;
-        if (len <= 1)
-            this.callButtonHidden = true;
         this.phoneNumber = this.phoneNumber.substring(0, len - 1);
-    }
-
-    removeAll() {
-        this.phoneNumber = '';
-        this.callButtonHidden = true;
     }
 
     call() {
         this.navCtrl.push(CallingPage, {'phoneNumber': this.phoneNumber});
+    }
+
+    personAdd() {
+      console.log('Add contact with number %s', this.phoneNumber);
     }
 }
