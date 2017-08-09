1a64a7782855bcf556496283c465a781c07aeb2a
Jordi <jordi.burguet.castell@gmail.com>
2017-08-09 04:40:50 +0200

    get call prices from the server

--- src/pages/call/calling.ts
+++ src/pages/call/calling.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AppState } from '../../store/app.state';
 import { Store } from '@ngrx/store';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-calling',
@@ -9,12 +11,31 @@ import { Store } from '@ngrx/store';
 })
 export class CallingPage {
   phoneNumber: string = '';
+  rates = {gsm: 'unknown', voip: 'unknown'};
 
-    constructor(public navCtrl: NavController,
-                public navParams: NavParams,
-                public store: Store<AppState>) {
-       this.phoneNumber = this.navParams.get('phoneNumber');
-    }
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public http: Http,
+              public store: Store<AppState>) {
+    this.phoneNumber = this.navParams.get('phoneNumber');
+
+    var name = 'joel';
+    var pass = 'joel1234';
+    let headers = new Headers();
+    headers.append('Authorization', 'Basic ' + btoa(name + ':' + pass));
+    var url = `http://dev.eyeseetea.com:5000/callPricing/${this.phoneNumber}`;
+    console.log(url);
+    this.http.get(url, {headers: headers}).map(
+      res => res.json()).subscribe(data => {
+        if (data.status == 'success') {
+          this.rates = data.data;
+        }
+        else {
+          console.log('meh');
+          console.log(data);
+        }
+    });
+  }
 
   gsm() {
     console.log('Tried to call %s using GSM', this.phoneNumber);
