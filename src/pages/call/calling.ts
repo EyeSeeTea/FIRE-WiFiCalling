a8ed1649edf65f6b7b1dcb0e831e13423fe120d2
Jordi <jordi.burguet.castell@gmail.com>
2017-08-24 04:18:18 +0200

    calling.ts: do the REST communication in ionViewDidLoad instead of the constructor, and present error messages as alerts

--- src/pages/call/calling.ts
+++ src/pages/call/calling.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
 import { Store } from '@ngrx/store';
+import { AlertController } from 'ionic-angular';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -17,26 +18,40 @@ export class CallingPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public http: Http,
+              private alertCtrl: AlertController,
               public store: Store<any>) {
     this.phoneNumber = this.navParams.get('phoneNumber');
+  }
 
+  ionViewDidLoad() {
     var name = 'joel';
     var pass = 'joel1234';
     let headers = new Headers();
     headers.append('Authorization', 'Basic ' + btoa(name + ':' + pass));
     var url = `http://dev.eyeseetea.com:5000/callPricing/${this.phoneNumber}`;
-    this.http.get(url, {headers: headers}).map(
-      res => res.json()).subscribe(data => {
+    this.http.get(url, {headers: headers}).map(res => res.json()).subscribe(
+      data => {
         if (data.status == 'success') {
           this.rates = data.data;
         }
         else {
-          console.log('Trouble with the data');
+          let alert = this.alertCtrl.create({
+            title: 'Trouble with the data',
+            subTitle: 'But the connection to the server worked...',
+            buttons: ['Damn']
+          });
+          alert.present();
           console.log(data);
         }
-    }), (err) => {
-        console.log('Authorization failed');
-    };
+      },
+      err => {
+        let alert = this.alertCtrl.create({
+          title: 'Authorization Error',
+          subTitle: err,
+          buttons: ['Damn']
+        });
+        alert.present();
+      });
   }
 
   gsm() {
